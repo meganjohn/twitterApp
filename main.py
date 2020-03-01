c83ed73de19ba038646d7ff79a084448f0286e5a
Megan John <meganjohn.791@gmail.com>
2020-03-01 15:10:38 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_input() -> str:
+    pass
+
+def get_tweets(hashtag: str) -> list:
+    pass
+
+def pick_tweets(all_tweets: list, number: int) -> list:
+    pass
+
+def output_tweets(selected_tweets: list) :
+    pass
+
+
+if __name__ == "__main__":
